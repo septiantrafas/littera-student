5169d622b50494175d719d4f233b8486160b7f88
Akramurridjal Rahman <xerod@outlook.com>
2021-04-01 21:33:49 +0700

    🧸 Apply lobby redirection using setTimeout

--- pages/[package]/lobby.tsx
+++ pages/[package]/lobby.tsx
@@ -1,13 +1,41 @@
 import Head from "next/head";
 
-import React from "react";
+import React, { useEffect } from "react";
 import PageWithLayoutType from "@/types/pageWithLayout";
 
 import Default from "@/layouts/default";
 import { Box, Flex, Progress, Spinner, Text } from "@chakra-ui/react";
 import { HiOutlineExclamation } from "react-icons/hi";
+import { GetServerSideProps } from "next";
+import { supabase } from "utils/initSupabase";
+import { useRouter } from "next/router";
 
-const Lobby: React.FC = () => {
+type ILobbyProps = {
+  paths: {
+    id: string;
+    package: { id: string };
+  };
+};
+
+const Lobby: React.FC<ILobbyProps> = ({ paths }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(paths);
+    setTimeout(() => {
+      router.push({
+        pathname: "/[package]/[section]",
+        query: {
+          package: paths.package.id,
+          section: paths.id,
+        },
+      });
+    }, 5000);
+    // return () => {
+    //   cleanup
+    // }
+  }, []);
+
   return (
     <>
       <Box maxW="100%" textAlign="center" overflow="hidden" m="10" p="10">
@@ -44,6 +72,28 @@ const Lobby: React.FC = () => {
       </Box>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const query = `
+  id,
+  package:package_id (id)
+  `;
+
+  const res = await supabase
+    .from<any>("sections")
+    .select(query)
+    .match({ package_id: context.params.package.toString() })
+    .order("number", { ascending: true })
+    .limit(1);
+
+  const data = res.data[0];
+  // Pass data to the page via props
+  return {
+    props: {
+      paths: data,
+    },
+  };
 };
 
 (Lobby as PageWithLayoutType).layout = Default;
