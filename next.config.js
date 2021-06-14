fe7618531dc61b9917ced8e2c1a095b1bbab894a
Akramurridjal Rahman <xerod@outlook.com>
2021-06-14 22:18:17 +0700

    📦 Update package.json

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   future: {
     webpack5: true,
   },
-};
+});
